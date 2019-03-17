55382f0da175055570e8e091ef8b3a8c3612eec6
Abhinav <abhinav.jonnada@gmail.com>
2019-03-17 10:22:47 -0500

    Major Optimization 2 DONE!

--- app.py
+++ app.py
@@ -48,9 +48,24 @@ class HelperBase():
         docs = store.collection(nameUser)
         return docs
 
+    # Loops over AutoID query and returns a dictionary
+    def loopOverAutoID(self, nameUser):
+        docs = self.queryAutoIDDoc(nameUser).where(u'Flag', u'==', True).get()
+        for doc in docs:
+            tmp = (doc.to_dict())
+        return tmp
+
+    # prints outstanding balance
+    def printInterest(self, nameUser):
+        docTot = store.collection(nameUser).document(u'total').get()
+        tmpDict = (docTot.to_dict())
+        outBal = tmpDict['OutStanding']
+        return outBal
+
 class ActionBase(ABC):
     def __init__(self, para):
         self.para = para
+
 
     def checkOptionEntered(self, para0, para1, para2):
         raise NotImplementedError("Subclass must implement this abstract method")
@@ -62,18 +77,23 @@ class ActionCenter(ActionBase):
     def checkOptionEntered(self, actionOption, nameUser, user):
         holder = HelperBase("holder")
 
+
 # User makes a transaction by manually entering data
         if (actionOption == 'Make'):
 
-            lis2 = []
+            lis2 = [] # list to hold outstanding balance!!
             date = int(input("Enter only date (03/dd/2019): \n"))
             reason = input("Enter your expense: \n")
             price = int(input("Enter Price: $ \n"))
+
             # Query to get outstanding balance of current transactions
             docs = holder.queryTotalDoc(nameUser).get()
             # converting object to dictionary and using key value pairs to get OutStanding bal.
             tmpDict = (docs.to_dict())
-            lis2.append(tmpDict['OutStanding'])
+            if tmpDict == None:
+                lis2.append(0)
+            else:
+                lis2.append(tmpDict['OutStanding'])
             b = sum(lis2) + price # current outstanding balance
             creditAvail = 1000 - b # updated credit limit
             # setting credit limit to $ 1000 for simplicity and makin sure user transactions are limited to $1000
@@ -82,7 +102,9 @@ class ActionCenter(ActionBase):
                     u'Date': date,
                     u'ExpenseType': reason,
                     u'ExpensePrice': price,
-                    u'OutStanding': b
+                    u'OutStanding': b,
+                    u'totVal': creditAvail,
+                    u'Flag': True
                 }
                 # Query for Document with AutoID
                 holder.queryAutoIDDoc(nameUser).add(data)
@@ -105,74 +127,80 @@ class ActionCenter(ActionBase):
         if (actionOption == 'View'):
             dateEntry = int(input("Enter only date (03/dd/2019): \n"))
             if (dateEntry == 30):
-                count = 0
-                lis = []
-                valIs = []
-                docs =  holder.queryAutoIDDoc(nameUser).where(u'Flag', u'==',True).get()
-                for doc in docs:
-                    tmp = (doc.to_dict())
-                    lis.append(tmp['Date'])
-                docs =  holder.queryAutoIDDoc(nameUser).where(u'Flag', u'==', True).get()
+
+                dataLis = []
+                interestAcc = []
+
+                tmp = holder.loopOverAutoID(nameUser)
+                dataLis.append(tmp['Date'])
+
+                #tmp1 = holder.loopOverAutoID(nameUser)
+                docs = holder.queryAutoIDDoc(nameUser).where(u'Flag', u'==', True).get()
                 for doc2 in docs:
                     tmp1 = (doc2.to_dict())
+
                     x= tmp1['OutStanding']
                     y= tmp1['Date']
-                    for i in range(len(lis)):
-                        if i == 0 and lis[i] == y:
-                            if len(lis) == 1:
+                    for i in range(len(dataLis)):
+                        if i == 0 and dataLis[i] == y:
+                            if len(dataLis) == 1:
                                 t = x * (0.35/365) * (30)
-                                valIs.append(t)
+                                interestAcc.append(t)
                                 break
                             else:
-                                t = x * (0.35 / 365) * (30-lis[i+1])
-                                valIs.append(t)
+                                t = x * (0.35 / 365) * (30-dataLis[i+1])
+                                interestAcc.append(t)
                                 break
-                        elif i == len(lis)-1 and lis[i] == y:
-                            t = x * (0.35 / 365) * (30 - lis[i])
-                            valIs.append(t)
+                        elif i == len(dataLis)-1 and dataLis[i] == y:
+                            t = x * (0.35 / 365) * (30 - dataLis[i])
+                            interestAcc.append(t)
                             break
-                        elif lis[i] == y:
-                            t = x * (0.35 / 365) * (lis[i+1]-lis[i])
-                            valIs.append(t)
+                        elif dataLis[i] == y:
+                            t = x * (0.35 / 365) * (dataLis[i+1]-dataLis[i])
+                            interestAcc.append(t)
                             break
+                print(interestAcc)
 
-                docTot = holder.queryTotalDoc(nameUser).document(u'total').get()
-                tmpDict = (docTot.to_dict())
-                outBal = tmpDict['OutStanding']
-                print("Intrest at end of 30th day is $ {}".format(sum(valIs) + outBal))
+
+                outBalance = holder.printInterest(nameUser)
+                print("Intrest at end of 30th day is $ {}".format(sum(interestAcc) + outBalance))
 
             else:
-                docs1 = holder.queryTotalDoc(nameUser).document(u'total').get()
-                tmpDict = (docs1.to_dict())
-                outBal = tmpDict['OutStanding']
-                print("Intrest on {} March 2019 is $ {}".format(dateEntry, outBal))
+                outBalance = holder.printInterest(nameUser)
+                print("Intrest on {} March 2019 is $ {}".format(dateEntry, outBalance))
 
             holder = HelperBase("holder")
             holder.responseHolderfun(actionOption, nameUser, user)
 
 
         if (actionOption == 'Pay'):
-            lis2 = []
+            # for better contrast, I have directly used queries, instead of helper functions
+            lis2 = [] # holds outstanding value
             date = int(input("Enter only date (03/dd/2019): \n"))
             payment = int(input("Enter amount to be payed: $ \n"))
-            docs = store.collection(nameUser).where(u'ExpensePrice', u'>=', 0).get()
-            for doc in docs:
-                tmpDict = (doc.to_dict())
-                lis2.append(tmpDict['ExpensePrice'])
+            # query returns outStanding balance from total document and reutrns updated balance
+            docs = store.collection(nameUser).document(u'total').get()
+            tmpDict = (docs.to_dict())
+            lis2.append(tmpDict['OutStanding'])
             b = sum(lis2) - payment  # update outstanding
 
             data = {
                 u'Date': date,
                 u'Payment': payment,
-                u'OutStanding': b
+                u'OutStanding': b,
+                u'Flag': True
             }
+            # query adds a transaction
             store.collection(nameUser).add(data)
             print("Transaction Successfull")
             print("Outstanding balance $ {}".format(b))
+
+            # query updates total document
             docTot = store.collection(nameUser).document(u'total')
             currentTotal = 1000 - b
             docTot.update({u'totVal': currentTotal, u'Date': date, u'OutStanding': b})
             print("Your available credit is $ {}".format(currentTotal))
+
             holder = HelperBase("holder")
             holder.responseHolderfun(actionOption, nameUser, user)
 
